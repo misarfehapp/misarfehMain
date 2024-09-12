27638a3bfaca69e346dcc03277b1d5160c17b183
ferpheri <dehghanian77@gmail.com>
2024-09-12 11:16:51 +0330

    finished user header

---
+++ app/user/page.tsx
@@ -0,0 +1,9 @@
+import UserHeader from "@/components/user header/UserHeader";
+
+const UserPage = () => {
+  return <div>
+    {/* header */}
+    <div><UserHeader name="محمد رنجبر" phone="0992450090"/></div>
+  </div>;
+};
+export default UserPage;
