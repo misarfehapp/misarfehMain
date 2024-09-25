83f02ce9f58ab2a7ede0ca355518c0996fb5fcf3
ferpheri <dehghanian77@gmail.com>
2024-09-25 09:52:41 +0330

    added rules section to user profile

--- components/user selection/UserSelection.tsx
+++ components/user selection/UserSelection.tsx
@@ -2,17 +2,35 @@ import Link from "next/link";
 import ChevronLeftIcon from "./ChevronLeftIcon";
 import UserIcon from "./UserIcon";
 import LocationIcon from "../address section/LocationIcon";
+import DollarIcon from "./DollarIcon";
+import HeadphoneIcon from "./HeadphoneIcon";
+import AlertIcon from "./AlertIcon";
 
 const sections = [
   {
     name: "اطلاعات کاربری",
     icon: <UserIcon />,
-    href:'information'
+    href: "information",
   },
   {
     name: "آدرس های شما",
     icon: <LocationIcon />,
-    href:'addresses'
+    href: "addresses",
+  },
+  {
+    name: "کیف پول",
+    icon: <DollarIcon />,
+    href: "/",
+  },
+  {
+    name: "پشتیبانی",
+    icon: <HeadphoneIcon />,
+    href: "/support",
+  },
+  {
+    name: "قوانین و شرایط",
+    icon: <AlertIcon />,
+    href: "/rules",
   },
 ];
 
@@ -22,13 +40,15 @@ const UserSelection = () => {
       <div className="w-full h-[6px] bg-neutral-neutral90" />
       {sections.map((section, index) => (
         <Link
-        href={`userProfile/${section.href}`}
+          href={`userProfile/${section.href}`}
           className="flex flex-row bg-neutral-neutral95 my-6 mx-4 min-h-[52px] justify-between items-center rounded-rounded-6"
           key={index}
         >
           <ChevronLeftIcon className="mx-3" />
           <div className="flex flex-row gap-3 justify-center items-center mx-3">
-            <p className="text-sm text-neutral-neutral30 font-bold">{section.name}</p>
+            <p className="text-sm text-neutral-neutral30 font-bold">
+              {section.name}
+            </p>
             <div>{section.icon}</div>
           </div>
         </Link>
