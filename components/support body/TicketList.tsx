a10dd70ec4379a3da78f36814ffa31a6950649d1
ferpheri <dehghanian77@gmail.com>
2024-09-26 11:53:18 +0330

    added scroll area for ticket list

--- components/support body/TicketList.tsx
+++ components/support body/TicketList.tsx
@@ -3,6 +3,7 @@
 import { useState } from "react";
 import CheckIcon from "../input/CheckIcon";
 import ClockIcon from "./ClockIcon";
+import { ScrollArea } from "../ui/scroll-area";
 
 const tickets = [
   {
@@ -61,57 +62,59 @@ const TicketList = () => {
     setIsExpanded((prev: any) => !prev);
   };
   return (
-    <div className="w-full my-6 flex flex-col justify-center items-center gap-4">
-      {tickets.map((ticket, index) => (
-        <div
-          key={index}
-          className="ring-2 w-full outline-none ring-neutral-neutral90 rounded-rounded-9 bg-primary-primary99 p-3 flex flex-col justify-center gap-2"
-          style={{ direction: "rtl" }}
-        >
-          {/* header of ticket */}
-          <div className="flex flex-row justify-between">
-            <p className="text-sm font-bold text-neutral-neutral30">
-              {ticket.subject}
-            </p>
-            {ticket.progress === "done" ? (
-              <div className="bg-primary-primary95 text-[#006000] flex flex-row items-center rounded-full text-xs font-medium py-1 px-2 gap-2">
-                <CheckIcon />
-                <p>حل شده</p>
-              </div>
-            ) : (
-              <div className="bg-neutral-neutral95 text-neutral-neutral30 flex flex-row items-center rounded-full text-xs font-medium py-1 px-2 gap-2">
-                <ClockIcon />
-                <p>پیگیری</p>
-              </div>
-            )}
-          </div>
-          {/* body of ticket */}
-          <div>
-            <p className="text-xs font-medium text-neutral-neutral35">
-              {ticket.discription.length > 100 && isExpanded === false
-                ? `${ticket.discription.slice(0, 100)}...`
-                : ticket.discription}
-              {ticket.discription.length > 100 && (
-                <span
-                  className="text-[#006000] cursor-pointer"
-                  onClick={toggleExpand}
-                >
-                  {isExpanded ? " کمتر" : " بیشتر"}
-                </span>
+    <ScrollArea className="h-[480px]">
+      <div className=" my-6 flex flex-col mx-4 justify-center items-center gap-4">
+        {tickets.map((ticket, index) => (
+          <div
+            key={index}
+            className="ring-2 w-full outline-none ring-neutral-neutral90 rounded-rounded-9 bg-primary-primary99 p-3 flex flex-col justify-center gap-2"
+            style={{ direction: "rtl" }}
+          >
+            {/* header of ticket */}
+            <div className="flex flex-row justify-between">
+              <p className="text-sm font-bold text-neutral-neutral30">
+                {ticket.subject}
+              </p>
+              {ticket.progress === "done" ? (
+                <div className="bg-primary-primary95 text-[#006000] flex flex-row items-center rounded-full text-xs font-medium py-1 px-2 gap-2">
+                  <CheckIcon />
+                  <p>حل شده</p>
+                </div>
+              ) : (
+                <div className="bg-neutral-neutral95 text-neutral-neutral30 flex flex-row items-center rounded-full text-xs font-medium py-1 px-2 gap-2">
+                  <ClockIcon />
+                  <p>پیگیری</p>
+                </div>
               )}
-            </p>
+            </div>
+            {/* body of ticket */}
+            <div>
+              <p className="text-xs font-medium text-neutral-neutral35">
+                {ticket.discription.length > 100 && isExpanded === false
+                  ? `${ticket.discription.slice(0, 100)}...`
+                  : ticket.discription}
+                {ticket.discription.length > 100 && (
+                  <span
+                    className="text-[#006000] cursor-pointer"
+                    onClick={toggleExpand}
+                  >
+                    {isExpanded ? " کمتر" : " بیشتر"}
+                  </span>
+                )}
+              </p>
+            </div>
+            {/* ticket footer */}
+            <div>
+              <p className="flex flex-row items-center text-xs font-medium text-neutral-neutral35 gap-1">
+                {ticket.time}
+                <span className="text-[#006000]">-</span>
+                {ticket.date}
+              </p>
+            </div>
           </div>
-          {/* ticket footer */}
-          <div>
-            <p className="flex flex-row items-center text-xs font-medium text-neutral-neutral35 gap-1">
-              {ticket.time}
-              <span className="text-[#006000]">-</span>
-              {ticket.date}
-            </p>
-          </div>
-        </div>
-      ))}
-    </div>
+        ))}
+      </div>
+    </ScrollArea>
   );
 };
 export default TicketList;
